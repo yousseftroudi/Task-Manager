a2ec6a1ad2051c3a536fd594a52c9a16bd42f1f5
unknown <yousseftroudi5@gmail.com>
2023-12-12 22:47:36 +0100

    Your commit message

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import './App.css';
+
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const handleInputChange = (e) => {
+    setNewTask(e.target.value);
+  };
+
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, { id: uuidv4(), text: newTask }]);
+      setNewTask('');
+    }
+  };
+
+  const removeTask = (taskId) => {
+    const updatedTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div className="App">
+      <h1>Task Manager</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Add a new task"
+          value={newTask}
+          onChange={handleInputChange}
+        />
+        <button onClick={addTask}>Add</button>
+      </div>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            {task.text}
+            <button onClick={() => removeTask(task.id)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
